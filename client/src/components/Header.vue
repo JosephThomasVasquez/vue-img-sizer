9d8817b44a1322ed57f545ef33d31f815f7960b5
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-23 15:11:53 -0700

    Created Header.vue component

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,20 +1,12 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div class="header">
     
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'Header',
 }
 </script>
 
