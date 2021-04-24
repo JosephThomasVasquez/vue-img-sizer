36cf28979817237b1408d3c1bdf6eaf172f3528b
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-24 15:00:19 -0700

    Added a FilesList vue

--- src/components/ImageForm.vue
+++ src/components/ImageForm.vue
@@ -3,15 +3,19 @@
     <h3>Choose a file to edit.</h3>
     <form v-on:submit.prevent="onSubmit" class="image-form-">
         <label for="file-selector-default" class="file-selector-label">Browse
-        <input type="file" id="file-selector-default" @change="getFile">
+        <input type="file" id="file-selector-default" @change="getFile" multiple>
         </label>
         <input type="text" id="file-selector-name" placeholder="Filename" v-bind:value="fileName">
     </form>
+    <FilesList />
     
   </div>
 </template>
 
 <script>
+
+import FilesList from './FilesList.vue';
+
 export default {
   name: 'ImageForm',
   data() {
@@ -22,7 +26,11 @@ export default {
   methods: {
       getFile(e) {
           console.log(e.target.value);
+          console.log(e.target.files);
       }
+  },
+  components: {
+      FilesList
   }
 }
 </script>
