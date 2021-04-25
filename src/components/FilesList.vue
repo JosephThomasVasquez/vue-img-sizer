88dd5ff5a303f3331fce528798d711f48f983628
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-24 18:31:04 -0700

    Converted filesizes to byte sets

--- src/components/FilesList.vue
+++ src/components/FilesList.vue
@@ -2,7 +2,7 @@
     <div>
         <h3 v-if="files.length !== 0">{{files.length}} Files</h3>
         <ul>
-            <li v-for="file in files" v-bind:key="file">{{file.name}} {{file.type}} {{file.size}}</li>
+            <li v-for="file in files" v-bind:key="file">{{file.name}} {{file.type}} {{fileSize(file.size)}}</li>
         </ul>
     </div>
 </template>
@@ -14,6 +14,14 @@
             files: Array
         },
         methods: {
+            fileSize(size, decimals) {
+                if (size === 0) return '0 Bytes';
+                const k = 1024;
+                const dm = decimals < 0 ? 0 : decimals;
+                const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+                const i = Math.floor(Math.log(size) / Math.log(k));
+                return parseFloat((size / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+            },
             listFiles(files) {
                 if (!files || files === null) {
                     return 'No Files';
